69257e9090adfc61f2e7fa34d210f17d8d395153
Awesomerly <Awesomerly@users.noreply.github.com>
2025-01-26 21:42:41 -0500

    Remove unused import

--- CleanOsu/Program.cs
+++ CleanOsu/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
